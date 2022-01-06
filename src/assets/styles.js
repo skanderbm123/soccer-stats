824c690ed5ef213d3420ff9d11705c89ff9493ff
Skander <skander96@hotmail.com>
2022-01-06 18:05:34 -0500

    test class welcome

--- src/assets/styles.js
+++ src/assets/styles.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { createGlobalStyle } from 'styled-components';
+import styled from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;900&display=swap');
@@ -26,3 +27,133 @@ export const GlobalStyle = createGlobalStyle`
       scroll-behavior: smooth;
   }
 `;
+
+
+export const MainBody = styled.div`
+  aligh-items: center;
+  display: flex;
+  flex-flow: column;
+  grid-gap: 10px 10px;
+  margin-bottom: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1600px;
+  min-width: 250px;
+  padding-top: 5px;
+  width: 80vw;
+`;
+
+export const ClubInformationSection = styled.div`
+  aligh-items: center;
+  display: flex;
+  flex-flow: row;
+  grid-gap: 10px 10px;
+  margin-left: auto;
+  margin-right: auto;
+  max-height: 365px;
+  max-width: 1600px;
+  min-width: 250px;
+  width: 80vw;
+`;
+
+export const TeamPlayersContainer = styled.div`
+  max-height: 300px;
+  width: 100%;
+`;
+
+export const TeamPlayersTabelContainer = styled.div`
+  background-color: #fff;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  display: flex;
+  flex-flow: column;
+  max-height: 325px;
+  min-width: 100%;
+  overflow-x: hidden;
+
+  ::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 9px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #00d4b1;
+    border: 1px solid #f1f3f4;
+    border-radius: 4px;
+  }
+`;
+
+export const TeamPlayersTabelHeader = styled.div`
+  background-color: #fff;
+  border-bottom: 1px solid #b2b2b2;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  display: grid;
+  font-weight: 700;
+  grid-template-areas: "name position nationality age height weight";
+  grid-template-columns: 3fr 2fr 2fr 1fr 1fr 1fr;
+  min-width: 100%;
+  padding: 5px;
+  padding-top: 10px;
+`;
+
+export const TeamPlayersTabelRow = styled.div`
+  background-color: #fff;
+  border-top: 1px solid #f1f3f4;
+  cursor: pointer;
+  display: grid;
+  font-weight: 400;
+  grid-template-areas: "name position nationality age height weight";
+  grid-template-columns: 3fr 2fr 2fr 1fr 1fr 1fr;
+  padding: 5px;
+
+  &:hover {
+    background-color: #00d4b1;
+  }
+`;
+
+export const PlayerName = styled.div`
+  display: inline-block;
+  grid-area: name;
+  height: 20px;
+  padding-left: 10px;
+`;
+
+export const PlayerPosition = styled.div`
+  grid-area: position;
+  text-align: center;
+`;
+
+export const PlayerNationality = styled.div`
+  grid-area: nationality;
+  text-align: center;
+`;
+
+export const PlayerAge = styled.div`
+  grid-area: age;
+  text-align: center;
+`;
+
+export const PlayerHeight = styled.div`
+  grid-area: height;
+  text-align: center;
+`;
+
+export const PlayerWeight = styled.div`
+  grid-area: weight;
+  text-align: center;
+`;
+
+export const PlayerFirstName = styled.div`
+  display: inline-block;
+  margin: auto;
+  max-height: 100%;
+`;
+
+export const PlayerLastName = styled.div`
+  display: inline-block;
+  margin: auto;
+  max-height: 100%;
+  padding-left: 10px;
+`;
+
--- src/components/SoccerStats.jsx
+++ src/components/SoccerStats.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import { GlobalStyle } from "../assets/styles";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
@@ -12,7 +10,7 @@ import ClubInfomation from "./ClubInfo";
 import TeamFixtures from "./TeamFixtures";
 import TeamPlayers from "./TeamPlayers";
 import PlayerHighlightedStats from "./PlayerHighlightedStats";
-import LiveScore from "./LiveScore";
+import Welcome from "./Welcome";
 
 import {
   getFootballStandings,
@@ -31,133 +29,22 @@ import {
   assignLiveScore,
 } from "../lib/CountriesAndLeagues";
 
-const MainBody = styled.div`
-  aligh-items: center;
-  display: flex;
-  flex-flow: column;
-  grid-gap: 10px 10px;
-  margin-bottom: 100px;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 1600px;
-  min-width: 250px;
-  padding-top: 5px;
-  width: 80vw;
-`;
-
-const ClubInformationSection = styled.div`
-  aligh-items: center;
-  display: flex;
-  flex-flow: row;
-  grid-gap: 10px 10px;
-  margin-left: auto;
-  margin-right: auto;
-  max-height: 365px;
-  max-width: 1600px;
-  min-width: 250px;
-  width: 80vw;
-`;
-
-const TeamPlayersContainer = styled.div`
-  max-height: 300px;
-  width: 100%;
-`;
-
-const TeamPlayersTabelContainer = styled.div`
-  background-color: #fff;
-  border-bottom-left-radius: 4px;
-  border-bottom-right-radius: 4px;
-  display: flex;
-  flex-flow: column;
-  max-height: 325px;
-  min-width: 100%;
-  overflow-x: hidden;
-
-  ::-webkit-scrollbar {
-    -webkit-appearance: none;
-    width: 9px;
-  }
-
-  ::-webkit-scrollbar-thumb {
-    background-color: #00d4b1;
-    border: 1px solid #f1f3f4;
-    border-radius: 4px;
-  }
-`;
-
-const TeamPlayersTabelHeader = styled.div`
-  background-color: #fff;
-  border-bottom: 1px solid #b2b2b2;
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-  display: grid;
-  font-weight: 700;
-  grid-template-areas: "name position nationality age height weight";
-  grid-template-columns: 3fr 2fr 2fr 1fr 1fr 1fr;
-  min-width: 100%;
-  padding: 5px;
-  padding-top: 10px;
-`;
-
-const TeamPlayersTabelRow = styled.div`
-  background-color: #fff;
-  border-top: 1px solid #f1f3f4;
-  cursor: pointer;
-  display: grid;
-  font-weight: 400;
-  grid-template-areas: "name position nationality age height weight";
-  grid-template-columns: 3fr 2fr 2fr 1fr 1fr 1fr;
-  padding: 5px;
-
-  &:hover {
-    background-color: #00d4b1;
-  }
-`;
-
-const PlayerName = styled.div`
-  display: inline-block;
-  grid-area: name;
-  height: 20px;
-  padding-left: 10px;
-`;
-
-const PlayerPosition = styled.div`
-  grid-area: position;
-  text-align: center;
-`;
-
-const PlayerNationality = styled.div`
-  grid-area: nationality;
-  text-align: center;
-`;
-
-const PlayerAge = styled.div`
-  grid-area: age;
-  text-align: center;
-`;
-
-const PlayerHeight = styled.div`
-  grid-area: height;
-  text-align: center;
-`;
-
-const PlayerWeight = styled.div`
-  grid-area: weight;
-  text-align: center;
-`;
-
-const PlayerFirstName = styled.div`
-  display: inline-block;
-  margin: auto;
-  max-height: 100%;
-`;
-
-const PlayerLastName = styled.div`
-  display: inline-block;
-  margin: auto;
-  max-height: 100%;
-  padding-left: 10px;
-`;
+import { GlobalStyle , 
+  MainBody, 
+  ClubInformationSection,
+  TeamPlayersContainer,
+  TeamPlayersTabelContainer,
+  TeamPlayersTabelHeader,
+  TeamPlayersTabelRow,
+  PlayerName,
+  PlayerPosition,
+  PlayerHeight,
+  PlayerNationality,
+  PlayerAge,
+  PlayerWeight,
+  PlayerFirstName,
+  PlayerLastName,
+ } from "../assets/styles";
 
 class SoccerStats extends Component {
   constructor() {
@@ -325,7 +212,9 @@ class SoccerStats extends Component {
             </TeamPlayersContainer>
           </TabPanel>
 
-          <TabPanel></TabPanel>
+          <TabPanel>
+          <Welcome name="Sara" />
+          </TabPanel>
 
           <TabPanel>
             <ClubInformationSection>
